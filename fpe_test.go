1d645e0399b68ca0f0082df461f5f03317e27309
Daniel Adams <daniel.adams@genworth.com>
2020-11-22 21:11:03 -0500

    test

--- fpe_test.go
+++ fpe_test.go
@@ -2,11 +2,14 @@ package shamirfpe
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-func Test_NewCipher(t *testing.T) {
+func Test_NewCipherNoError(t *testing.T) {
 	radix := 10   // is this correct for tokenizing digits?
 	maxTLen := 32 // max 256 bit tweak
 	tweak := []byte("thisisaknowntweakthisisaknowntwe")
-	NewCipher(radix, maxTLen, tweak)
+	_, err := NewCipher(radix, maxTLen, tweak)
+	require.NoError(t, err)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/danieladams456/shamirfpe
 
 go 1.15
 
-require github.com/capitalone/fpe v1.2.1
+require (
+	github.com/capitalone/fpe v1.2.1
+	github.com/stretchr/testify v1.6.1
+)
