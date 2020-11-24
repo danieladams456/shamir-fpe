a2592fce3c3c39c54e2da0176fa93ced03bfab0d
Daniel Adams <daniel.adams@genworth.com>
2020-11-23 21:20:23 -0500

    end to end test

--- shamirfpe_test.go
+++ shamirfpe_test.go
@@ -9,17 +9,32 @@ import (
 )
 
 // This is just a test unrelated to the library to understand how the vault shamir project works
-func Test_SplitCombine(t *testing.T) {
+func Test_EndToEnd(t *testing.T) {
 	desiredSecret := []byte("thisisasecretkeythisisasecretkey")
 	desiredNumParts := 5
 	desiredThreshold := 2
 	parts, err := shamir.Split(desiredSecret, desiredNumParts, desiredThreshold)
 	require.NoError(t, err)
 
+	sf := shamirfpe.ShamirFpe{}
 	// combine first desiredThreshold keys
-	combinedSecret, err := shamir.Combine(parts[:desiredThreshold])
+	for _, v := range parts[:desiredThreshold] {
+		sf.AddKeyPart(v)
+	}
+
+	// construct shamirfpe
+	radix := 36   // for tokenizing digits + letters (case insensitive)
+	maxTLen := 32 // max 256 bit tweak
+	tweak := []byte("test_tweak")
+	c, err := sf.NewCipher(radix, maxTLen, tweak)
 	require.NoError(t, err)
-	require.Equal(t, desiredSecret, combinedSecret)
+
+	plaintext := "testplaintext"
+	ciphertext, err := c.Encrypt(plaintext)
+	require.NoError(t, err)
+	decrypted, err := c.Decrypt(ciphertext)
+	require.NoError(t, err)
+	require.Equal(t, plaintext, decrypted)
 }
 
 func Test_NewCipherErrorsNonEqualLength(t *testing.T) {
